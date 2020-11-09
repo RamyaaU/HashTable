ed375972ff06bf44087874e30933263d7b26b724
Ramya.U <ramyayadav2016@gmail.com>
2020-11-09 14:27:26 +0530

    [Ramya] Add . Added method to find frequency using linked list to do hash operation

---
+++ HashTable/MyMapNode.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class MyMapNode<K, V>
+    {
+        //defining variables
+        public readonly int size;
+        public readonly LinkedList<keyValue<K, V>>[] items;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyMapNode{K, V}"/> class.
+        /// </summary>
+        /// <param name="size">The size.</param>
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<keyValue<K, V>>[size];
+        }
+
+        /// <summary>
+        /// get set methods 
+        /// </summary>
+        /// <typeparam name="k"></typeparam>
+        /// <typeparam name="v"></typeparam>
+        public struct keyValue<k, v>
+        {
+            public k key { get; set; }
+            public v value { get; set; }
+        }
+
+        /// <summary>
+        /// Gets the linkedlist.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <returns></returns>
+        // Creating a GetLinkedList Method with return type as KeyValue Pair
+        // Gets the linked list present at the entered position in the items[] array
+        protected LinkedList<keyValue<K, V>> GetLinkedlist(int position)
+        {
+            LinkedList<keyValue<K, V>> linkedlist = items[position];
+
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<keyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+
+        /// <summary>
+        /// Gets the array position.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        /// to Find the Array Position with key as the parameter
+        protected int GetArrayPosition(K key)
+        {
+            //gethashcode gives position value using inbuilt function
+            int position = key.GetHashCode() % size;
+            //returning absolute position
+            return Math.Abs(position);
+        }
+
+        /// <summary>
+        /// Gets the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        // Gets the value at the specified key.
+        // Generic Method to find the key value pair at the particular position
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+
+            LinkedList<keyValue<K, V>> linkedlist = GetLinkedlist(position);
+
+            foreach (keyValue<K, V> item in linkedlist)
+            {
+                if (item.key.Equals(key))
+                {
+                    return item.value;
+                }
+            }
+            return default;
+        }
+
+        /// <summary>
+        /// Adds the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<keyValue<K, V>> linkedlist = GetLinkedlist(position);
+            keyValue<K, V> item = new keyValue<K, V>() { key = key, value = value };
+
+            linkedlist.AddLast(item);
+        }
+
+        /// <summary>
+        /// Removes the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<keyValue<K, V>> linkedlist = GetLinkedlist(position);
+
+            bool itemFound = false;
+
+            keyValue<K, V> foundItem = default(keyValue<K, V>);
+
+            foreach (keyValue<K, V> item in linkedlist)
+            {
+                if (item.key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedlist.Remove(foundItem);
+            }
+        }
+
+        // UC 1 : Prints the frequency of the specified value in the hashtable.        
+        /// <summary>
+        /// Gets the frequency.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public void GetFrequency(V value)
+        {
+            int frequency = 0;
+            // Iterating using the foreach loop to get the key value of each item
+            foreach (LinkedList<keyValue<K, V>> list in items)
+            {
+                // checking if key is not null 
+                if (list == null)
+                    continue;
+                // iterating using the foreach loop to get the value of the item in linked list
+                foreach (keyValue<K, V> obj in list)
+                {
+                    if (obj.Equals(null))
+                        continue;
+                    if (obj.value.Equals(value))
+                        frequency++;
+                }
+            }
+            Console.WriteLine("Frequency of {0} is {1}", value, frequency);
+        }
+    }
+}
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -10,7 +10,19 @@ namespace HashTable
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to HashMap!");
+            Console.WriteLine("Hello, Welcome to HashTable!");
+            //creating an object of mapnode class
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "To");
+            hash.Add("5", "be");
+            hash.GetFrequency("To");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value: " + hash5);
+
         }
     }
 }
